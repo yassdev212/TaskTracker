6705e7ad6dfe34ec940ab76123571f40b088f341
yassin dev <yassinabdallahatef@gmail.com>
2025-11-29 11:59:54 +0200

    task2: add tasks

--- TaskTracker/main.py
+++ TaskTracker/main.py
@@ -1,1 +1,15 @@
+tasks = []
+
 print ("welcome to tasktracker!")
+
+while True:
+    print("\nOptions: 1. Add Task  2. Exit")
+    choice = input("Choose: ")
+
+    if choice == '1':
+        task = input("Enter task name: ")
+        tasks.append(task)
+        print("Task added.")
+    
+    elif choice == '2':
+        break
