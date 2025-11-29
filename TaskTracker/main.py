fc68b13b4b96332c346e69471133e01dfac8d84f
yassin dev <yassinabdallahatef@gmail.com>
2025-11-29 12:01:50 +0200

    task3:ability to add tasks

--- TaskTracker/main.py
+++ TaskTracker/main.py
@@ -1,15 +1,20 @@
 tasks = []
 
-print ("welcome to tasktracker!")
+print("Welcome to TaskTracker!")
 
 while True:
-    print("\nOptions: 1. Add Task  2. Exit")
+    print("\nOptions: 1. Add Task  2. List Tasks  3. Exit") 
     choice = input("Choose: ")
 
     if choice == '1':
         task = input("Enter task name: ")
         tasks.append(task)
         print("Task added.")
-    
-    elif choice == '2':
+
+    elif choice == '2': 
+        print("\nYour Tasks:")
+        for t in tasks:
+            print(t)
+
+    elif choice == '3': 
         break
